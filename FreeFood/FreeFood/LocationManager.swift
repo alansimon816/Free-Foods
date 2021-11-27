71e9d4a0825bca80800c5cc5edd981b93da75d98
user <user@Mac101.verizon.net>
2021-11-27 16:42:24 -0500

    Creates LocationManager.swift and LocationRequestView.swift -Alan

---
+++ FreeFood/FreeFood/LocationManager.swift
@@ -0,0 +1,55 @@
+//
+//  LocationManager.swift
+//  FreeFood
+//
+//  Created by user on 11/27/21.
+//
+
+import CoreLocation
+
+
+class LocationManager: NSObject, ObservableObject {
+    @Published var userLocation: CLLocation?
+    private let lm = CLLocationManager()
+    static let shared = LocationManager() // Accessible anywhere in app
+    
+    // Standard location stuff
+    override init() {
+        super.init()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        lm.startUpdatingLocation()
+    }
+    
+    func requestLocation() {
+        lm.requestWhenInUseAuthorization() // only requests location when app is in use
+    }
+}
+
+// Needed for the line "lm.delegate = self" in above initializer
+extension LocationManager: CLLocationManagerDelegate {
+    // Manages the different possible location authorization statuses.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        switch status {
+        case .notDetermined:
+            print("<DEBUG> Not determined")
+        case .restricted:
+            print("<DEBUG> Restricted")
+        case .denied:
+            print("<DEBUG> Denied")
+        case .authorizedAlways:
+            print("<DEBUG> Authorized Always")
+        case .authorizedWhenInUse:
+            print("<DEBUG> Authorized When In Use")
+        @unknown default:
+            break
+        }
+    }
+    
+    // Manages location after user has agreed to share their location
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let loc = locations.last else { return }
+        self.userLocation = loc
+    }
+}
