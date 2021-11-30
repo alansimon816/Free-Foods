04213006610ccb49f36a9e02c992da1451359f35
Alan Simon <asimon21@terpmail.umd.edu>
2021-11-30 13:18:17 -0500

    adds submission history view

---
+++ FreeFood/FreeFood/Views/SubmissionHistoryView.swift
@@ -0,0 +1,34 @@
+//        NavigationView {
+//            VStack {
+//                Divider()
+//                NavigationLink(destination: SubmissionHistoryView()) {
+//                    HStack {
+//                        Image(systemName: "clock")
+//                        Text("Food Submission History")
+//                          //.navigationBarHidden(true)
+//                        Spacer()
+//                    }
+//                }
+//                Divider()
+//                NavigationLink(destination: EditAccountView()) {
+//                    HStack {
+//                        Image(systemName: "pencil")
+//                        Text("Edit Account Details")
+//                          //.navigationBarHidden(true)
+//                        Spacer()
+//                    }
+//                }
+//                Divider()
+//                NavigationLink(destination: AboutView()) {
+//                    HStack {
+//                        Image(systemName: "info.circle")
+//                        Text("About")
+//                          //.navigationBarHidden(true)
+//                        Spacer()
+//                    }
+//                }
+//                Divider()
+//                Spacer()
+//                LogOutButton()
+//            }
+//        }
